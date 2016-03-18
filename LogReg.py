637fed9e79097dfa66f234baa4f41d8c7b151e18
Alexander Samoylov <asamoylov@yandex-team.ru>
2016-03-18 20:18:00 +0300

    123

--- LogReg.py
+++ LogReg.py
@@ -7,6 +7,7 @@ Created on Sat Feb 27 00:00:57 2016
 
 import pandas
 import sklearn
+from sklearn import preprocessing,metrics
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -41,12 +42,23 @@ def Q_lg(x,y,w):
     return q
 
 def q_grad_lg(x,y,w):
-    tmp = np.dot( - y*(  1-1/( 1+np.exp(-y*np.sum(w*x, axis=1)) )  ), x)
+    tmp = np.dot( - y*(  1- 1 / ( 1 + np.exp( -y*np.sum(w*x, axis=1)) )  ), x)
     dq = tmp / len(x)
     x1=x[:,0]
     x2=x[:,1]
-    dw1 = sum((1-1/(1+np.exp(-y*np.sum(w*x, axis=1))))*y*x1) / len(x)
-    dw2 = sum((1-1/(1+np.exp(-y*np.sum(w*x, axis=1))))*y*x2) / len(x)
+    w1 = sum((1-1/(1+np.exp(-y*np.sum(w*x, axis=1))))*y*x1) / len(x)
+    w2 = sum((1-1/(1+np.exp(-y*np.sum(w*x, axis=1))))*y*x2) / len(x)
+    return dq
+
+def q_grad2_lg(x,y,w):
+    dq=np.zeros(x.shape[1])
+    for i in range(x.shape[1]):
+        tmp=0
+        for j in range(x.shape[0]):
+            tmp = tmp + (np.sum(w*x[j,:]) - y[j])*x[j,i]
+        dq[i] = tmp / len(x)
+        
+    #dq = np.dot( (np.sum(w*x, axis=1) - y), x ) / len(x)
     return dq
 
 def grad_descent_l2(x,y,w, k, c, grad, iters):
@@ -63,13 +75,14 @@ data = pandas.read_csv('data-logistic.csv', names=['target', 'x1', 'x2'])
 sclr = sklearn.preprocessing.StandardScaler()
 
 x = sclr.fit_transform(data[['x1', 'x2']])
+x = data[['x1','x2']].values
 y = data['target'].values
 w0 = np.zeros(x.shape[1])
 
 (w,qi) = grad_descent_l2(x,y,w0, 0.1, 10, q_grad_lg , 10000)
 
 a = 1 / (1+np.exp(-np.sum(w*x, axis=1)))
-print "%.4f" % sklearn.metrics.roc_auc_score(y,a)
+print "%.3f" % sklearn.metrics.roc_auc_score(y,a)
 
 plt.plot(qi)
 plt.show()
