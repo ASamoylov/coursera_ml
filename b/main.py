e67bebc90d6fc711f91a02f4f48166e57ff1c890
Alexander Samoylov <asamoylov@yandex-team.ru>
2016-05-17 18:28:45 +0300

    bb

--- b/main.py
+++ b/main.py
@@ -13,13 +13,13 @@ import Config
 from datetime import time
 
 
-time.resolution(second=1)
+#time.resolution(second=1)
 
 
-dbc = lite.connect("data.db")
-cur = dbc.cursor()
-cur.execute("create table my_ind (id varchar(15) primary key, desc varchar(50), value float, time integer)")
-dbc.commit()
+#dbc = lite.connect("data.db")
+#cur = dbc.cursor()
+#cur.execute("create table my_ind (id varchar(15) primary key, desc varchar(50), value float, time integer)")
+#dbc.commit()
 
 resp = urllib2.urlopen("http://moex.com/iss/engines/stock/markets/index/securities.json?securities=%s" % ",".join(Config.indexes))
 my_json = json.loads(resp.read())
